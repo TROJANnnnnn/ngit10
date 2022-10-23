ea3f2fb0ac5923aec828c07f8e197420cf1f460c
Trojan <93863728+TROJANnnnnn@users.noreply.github.com>
2022-10-23 15:09:10 +0530

    palindrome

---
+++ ngit10.py
@@ -0,0 +1,14 @@
+# function which return reverse of a string
+
+def isPalindrome(s):
+	return s == s[::-1]
+
+
+# Driver code
+s = "malayalam"
+ans = isPalindrome(s)
+
+if ans:
+print("Yes")'''ERROR - incorrect identation'''
+else:
+	print("No")
